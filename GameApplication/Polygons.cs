c58a7ae02de0b40b4cd0b87f227fed8c045a715c
mszauer <mszauer@mail.usf.edu>
2016-01-11 18:12:34 -0500

    finished ch3

--- GameApplication/Polygons.cs
+++ GameApplication/Polygons.cs
@@ -60,7 +60,6 @@ namespace GameApplication {
             GL.Vertex4(0.3, 0.25, 0, 1);
             GL.Vertex4(0.3, -0.25, 0, 1);
             GL.End();
-            */
             if (GL.IsEnabled(EnableCap.PolygonSmooth)) {
                 GL.Disable(EnableCap.PolygonSmooth);
             }
@@ -77,6 +76,86 @@ namespace GameApplication {
             GL.Vertex2(-0.10, 1);
             GL.Vertex2(-0.86, 1);
             GL.End();
+            */
+            float scale = 0.125f;
+
+            // Draw points
+            float xOffset = -0.9f;
+            float yOffset = 0.9f;
+            GL.PointSize(4.0f);
+            GL.Begin(PrimitiveType.Points);
+            for (int x = 0; x < 4; x++) {
+                for (int y = 0; y < 4; y++) {
+                    // We use 0.9 instead of 1 to go close to the edge, not all the way
+                    // Why are we multiplying 4 by 0.125 (scale)?
+                    // Because X and Y both go from 0 to 3, a total of 4 units
+                    GL.Vertex3(xOffset + x * scale, yOffset - y * scale, 0);
+                }
+            }
+            GL.End();
+
+            // Draw Triangle back facing
+            xOffset = -0.25f;
+            yOffset = 0.9f;
+            GL.Begin(PrimitiveType.Triangles);
+            for (int x = 0; x < 4; x++) {
+                for (int y = 0; y < 4; y++) {
+                    GL.Vertex3(xOffset + x * scale, yOffset - y * scale, 0);
+                    GL.Vertex3(xOffset + (x + 1) * scale, yOffset - y * scale, 0);
+                    GL.Vertex3(xOffset + x * scale, yOffset - (y + 1.0f) * scale, 0);
+                }
+            }
+            GL.End();
+
+            // Set polygons to render as lines
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
+
+            // Draw quads
+            xOffset = 0.9f;
+            yOffset = 0.9f;
+            GL.Begin(PrimitiveType.Quads);
+            for (int x = 0; x < 4; x++) {
+                for (int y = 0; y < 4; y++) {
+                    GL.Vertex3(xOffset - x * scale, yOffset - y * scale, 0);
+                    GL.Vertex3(xOffset - (x + 1) * scale, yOffset - y * scale, 0);
+                    GL.Vertex3(xOffset - (x + 1) * scale, yOffset - (y + 1) * scale, 0);
+                    GL.Vertex3(xOffset - x * scale, yOffset - (y + 1) * scale, 0);
+                }
+            }
+            GL.End();
+
+            // Draw triangle strip
+            xOffset = 0.15f;
+            yOffset = -0.5f;
+            for (int x = 0; x < 4; x++) {
+                GL.Begin(PrimitiveType.TriangleStrip);
+                for (int y = 0; y < 4; y++) {
+                    GL.Vertex3(xOffset + x * scale, yOffset + y * scale, 0);
+                    GL.Vertex3(xOffset + (x + 1) * scale, yOffset + y * scale, 0);
+                    GL.Vertex3(xOffset + x * scale, yOffset + (y + 1) * scale, 0);
+                    GL.Vertex3(xOffset + (x + 1) * scale, yOffset + (y + 1) * scale, 0);
+                }
+                GL.End();
+            }
+
+            // Draw triangle fan backward facing
+            xOffset = -0.15f;
+            yOffset = -0.5f;
+            GL.Begin(PrimitiveType.TriangleFan);
+            // Center of thefan
+            GL.Vertex3(xOffset - 0.0f, yOffset + 0.0f, 0.0f);
+            // Bottom side
+            for (int x = 5; x > 0; x--) {
+                GL.Vertex3(xOffset - (x - 1) * scale, yOffset + 4 * scale, 0);
+            }
+            // Right side
+            for (int y = 5; y > 0; y--) {
+                GL.Vertex3(xOffset - 4 * scale, yOffset + (y - 1) * scale, 0);
+            }
+            GL.End();
+
+            // Reset polygon modes for next render
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Fill);
         }
         public void Shutdown() {
 
