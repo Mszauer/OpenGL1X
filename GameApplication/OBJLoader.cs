f5ef5c47d6f45468a2e0b27c2cca8f2208ed0c29
mszauer <mszauer@mail.usf.edu>
2016-03-28 21:37:58 -0400

    working on objloader, need to finish render

---
+++ GameApplication/OBJLoader.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OpenTK.Graphics.OpenGL;
+
+namespace GameApplication {
+    class OBJLoader {
+        protected int vertexBuffer = -1;
+
+        protected bool hasNormals = false;
+        protected bool hasUvs = false;
+
+        protected int numVerts = 0;
+        protected int numNormals = 0;
+        protected int numUvs = 0;
+
+        public OBJLoader(string path) {
+            List<float> vertices = new List<float>();
+            List<float> normals = new List<float>();
+            List<float> texCoord = new List<float>();
+
+            List<uint> vertIndex = new List<uint>();
+            List<uint> normIndex = new List<uint>();
+            List<uint> uvIndex = new List<uint>();
+
+            List<float> vertexData = new List<float>();
+            List<float> normalData = new List<float>();
+            List<float> uvData = new List<float>();
+
+            using (TextReader reader = File.OpenText(path)) {
+                string line = reader.ReadLine();
+                while (line != null ) {
+                    if (string.IsNullOrEmpty(line)||line.Length < 2) {
+                        continue;
+                    }
+                    for (int i = 0; i < vertIndex.Count; i++) {
+                        vertexData.Add(vertices[(int)vertIndex[i] * 3 + 0]);
+                        vertexData.Add(vertices[(int)vertIndex[i] * 3 + 1]);
+                        vertexData.Add(vertices[(int)vertIndex[i] * 3 + 2]);
+                    }
+                    for (int i = 0; i < normIndex.Count; i++) {
+                        normalData.Add(normals[(int)normIndex[i] * 3 + 0]);
+                        normalData.Add(normals[(int)normIndex[i] * 3 + 1]);
+                        normalData.Add(normals[(int)normIndex[i] * 3 + 2]);
+                    }
+                    for (int i = 0; i < uvIndex.Count; i++) {
+                        uvData.Add(texCoord[(int)uvIndex[i] * 2 + 0]);
+                        uvData.Add(texCoord[(int)uvIndex[i] * 2 + 1]);
+                    }
+                    line = reader.ReadLine();
+                }
+                hasNormals = normalData.Count > 0;
+                hasUvs = uvData.Count > 0;
+
+                numVerts = vertexData.Count;
+                numUvs = uvData.Count;
+                numNormals = normalData.Count;
+
+                List<float> data = new List<float>();//create linear array of data
+                data.AddRange(vertexData); //add everything from vertData
+                data.AddRange(normalData);//add everything from normData
+                data.AddRange(uvData);//add everything from uvData
+
+                vertexBuffer = GL.GenBuffer();
+                GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBuffer);//bind array
+                GL.BufferData(BufferTarget.ArrayBuffer, new IntPtr(data.Count * sizeof(float)), data.ToArray(), BufferUsageHint.StaticDraw);//transfer array to gpu
+                GL.BindBuffer(BufferTarget.ArrayBuffer, 0);//release array
+            }
+        }
+        public void Destroy() {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
+
+            GL.DeleteBuffer(vertexBuffer);
+
+            vertexBuffer = -1;
+        }
+        public void Render(bool useNormals = true, bool useTextures = true) {
+            if (vertexBuffer == -1) {
+                return;
+            }
+            if (!hasNormals) {
+                useNormals = false;
+            }
+            if (!hasUvs) {
+                useTextures = false;
+            }
+            //enable client states , check arguments
+            //bind array buffer
+            //set pointers
+            //vertex ptr offset = 0;
+            //normal ptr offset = numberts * sizeof(float)
+            //uv ptr offset = (numverts+numnorms) * sizeof(float)
+
+            //call GL.DrawArrays, always triangles
+        }
+    }
+}
