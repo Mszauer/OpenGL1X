358ab008aa5f11e1e5beb8a70cf2176b681a266b
mszauer <mszauer@mail.usf.edu>
2016-03-31 18:48:22 -0400

    slightly worked on loading and parsing

--- GameApplication/OBJLoader.cs
+++ GameApplication/OBJLoader.cs
@@ -14,6 +14,8 @@ namespace GameApplication {
         protected int numNormals = 0;
         protected int numUvs = 0;
 
+        protected string materialPath = null;
+
         public OBJLoader(string path) {
             List<float> vertices = new List<float>();
             List<float> normals = new List<float>();
@@ -33,39 +35,73 @@ namespace GameApplication {
                     if (string.IsNullOrEmpty(line)||line.Length < 2) {
                         continue;
                     }
-                    for (int i = 0; i < vertIndex.Count; i++) {
-                        vertexData.Add(vertices[(int)vertIndex[i] * 3 + 0]);
-                        vertexData.Add(vertices[(int)vertIndex[i] * 3 + 1]);
-                        vertexData.Add(vertices[(int)vertIndex[i] * 3 + 2]);
+                    line.ToLower();
+                    string[] content = line.Split(' ');
+                    if (content[0] == "usemtl") {
+                        materialPath = content[1];
                     }
-                    for (int i = 0; i < normIndex.Count; i++) {
-                        normalData.Add(normals[(int)normIndex[i] * 3 + 0]);
-                        normalData.Add(normals[(int)normIndex[i] * 3 + 1]);
-                        normalData.Add(normals[(int)normIndex[i] * 3 + 2]);
+                    else if (content[0] == "v") {
+                        //add vertex
+                        foreach (string vertex in content) {
+                            
+                        }
                     }
-                    for (int i = 0; i < uvIndex.Count; i++) {
-                        uvData.Add(texCoord[(int)uvIndex[i] * 2 + 0]);
-                        uvData.Add(texCoord[(int)uvIndex[i] * 2 + 1]);
+                    else if (content[0] == "vt") {
+                        // vertex texture
+                        foreach (string vertex in content) {
+
+                        }
+                    }
+                    else if (content[0] == "vn") {
+                        //vertex normal
+                        foreach (string vertex in content) {
+
+                        }
+                    }
+                    else if (content[0] == "f") {
+                        //face
+                        //have all faces, split in x/y/z form
+                        //want linear array xyzxyzxyzxyz
+
+                    }
+                    else if (content[0] == "s") {
+                        //specular
                     }
                     line = reader.ReadLine();
                 }
-                hasNormals = normalData.Count > 0;
-                hasUvs = uvData.Count > 0;
-
-                numVerts = vertexData.Count;
-                numUvs = uvData.Count;
-                numNormals = normalData.Count;
-
-                List<float> data = new List<float>();//create linear array of data
-                data.AddRange(vertexData); //add everything from vertData
-                data.AddRange(normalData);//add everything from normData
-                data.AddRange(uvData);//add everything from uvData
-
-                vertexBuffer = GL.GenBuffer();
-                GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBuffer);//bind array
-                GL.BufferData(BufferTarget.ArrayBuffer, new IntPtr(data.Count * sizeof(float)), data.ToArray(), BufferUsageHint.StaticDraw);//transfer array to gpu
-                GL.BindBuffer(BufferTarget.ArrayBuffer, 0);//release array
+                
             }
+            for (int i = 0; i < vertIndex.Count; i++) {
+                vertexData.Add(vertices[(int)vertIndex[i] * 3 + 0]);
+                vertexData.Add(vertices[(int)vertIndex[i] * 3 + 1]);
+                vertexData.Add(vertices[(int)vertIndex[i] * 3 + 2]);
+            }
+            for (int i = 0; i < normIndex.Count; i++) {
+                normalData.Add(normals[(int)normIndex[i] * 3 + 0]);
+                normalData.Add(normals[(int)normIndex[i] * 3 + 1]);
+                normalData.Add(normals[(int)normIndex[i] * 3 + 2]);
+            }
+            for (int i = 0; i < uvIndex.Count; i++) {
+                uvData.Add(texCoord[(int)uvIndex[i] * 2 + 0]);
+                uvData.Add(texCoord[(int)uvIndex[i] * 2 + 1]);
+            }
+
+            hasNormals = normalData.Count > 0;
+            hasUvs = uvData.Count > 0;
+
+            numVerts = vertexData.Count;
+            numUvs = uvData.Count;
+            numNormals = normalData.Count;
+
+            List<float> data = new List<float>();//create linear array of data
+            data.AddRange(vertexData); //add everything from vertData
+            data.AddRange(normalData);//add everything from normData
+            data.AddRange(uvData);//add everything from uvData
+
+            vertexBuffer = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBuffer);//bind array
+            GL.BufferData(BufferTarget.ArrayBuffer, new IntPtr(data.Count * sizeof(float)), data.ToArray(), BufferUsageHint.StaticDraw);//transfer array to gpu
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);//release array
         }
         public void Destroy() {
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
